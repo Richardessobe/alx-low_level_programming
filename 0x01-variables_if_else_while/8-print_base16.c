b6ee169c89c8cad91c4cb042a062e46b298958cc
Richardessobe <muledimulema@gmail.com>
2022-09-11 19:24:07 -0700

    fix1

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,14 +15,14 @@ int main(void)
 	for (digit = '0'; digit < '9'; digit++)
 	{	
 		putchar(digit);
-	}
 
+	}
 	for (digit = 'a'; digit < 'f'; digit++)
 	{
 		putchar(digit);
 	
 	}
-
 	putchar('/n');
+
 	return (0);
 }	
