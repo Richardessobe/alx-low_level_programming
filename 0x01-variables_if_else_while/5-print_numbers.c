0d838438e8b26fafd806d7c2b98ca5f21572906e
Richardessobe <muledimulema@gmail.com>
2022-09-11 20:54:03 -0700

    task fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,7 +11,9 @@
 int main(void)
 
 {
+
 int digit;
+
 for (digit = 0; digit < 10; digit++)
 	printf("%i", digit);
 
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,12 @@
 int main(void)
 
 {
+
 int digit;
 
 for (digit = 0; digit = 9; digit++)
 	putchar(digit + '0');
+
 	if (digit <= 9)
 		putchar(',');
 		putchar(' ');
