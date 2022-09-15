fb519cbf40ea72c9a3859d89f0e4cecd77f8a87d
Richardessobe <muledimulema@gmail.com>
2022-09-15 07:46:21 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,19 +14,19 @@ int i, a;
 
 
 for (i = 0; i <= 9; i++)
+{
+
+	for (a = 97; a <= 122; a++)
+	{
 
 
-for (a = 97; a <= 122; a++)
-
-
-
-	_putchar(a);
-
+		_putchar(a);
+	}
 
 
 
 	_putchar('\n');
-
+}
 
 
 }
